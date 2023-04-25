f7a9a7e3bf3b76b6778d9c58e992a93af63dcb4d
zineb1098 <manssourizineb8@gmail.com>
2023-04-24 18:27:42 -0700

    Task11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,3 +14,18 @@ for (l = 50; l < 58; l++)
 {
 if (l > m && m > n)
 {
+putchar(n);
+putchar(m);
+putchar(l);
+if (n != 55 || m != 56)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
